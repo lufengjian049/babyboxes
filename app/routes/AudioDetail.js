420575df29b215bf81c3d5d545fc0c62b2d36c99
lufj <lufengjian0409@163.com>
2017-05-08 23:21:59 +0800

    third page

--- app/routes/AudioDetail.js
+++ app/routes/AudioDetail.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity, Slider } from 'react-native'
 import pageDecorator from '../hocs/PageDecorator'
 import LoadingSpinner from '../components/LoadingSpinner'
 import { Grid, Col } from 'react-native-elements'
@@ -12,9 +12,12 @@ import ViewPort from '../components/ViewPort'
   ...audio,
 }))
 class AudioDetail extends Component {
-  // constructor(props) {
-  //   super(props)
-  // }
+  constructor(props) {
+    super(props)
+    this.state = {
+        sliderValue:0,
+    }
+  }
   componentDidMount() {
     this.props.dispatch(this.props.createAction('audio/getAudioById')({ id: this.props.id }))
   }
@@ -34,20 +37,43 @@ class AudioDetail extends Component {
     }
     return (
       <ViewPort nobottom>
-        <View style={{flexGrow:1,flexBasis:180}}>
+        <View style={{flexGrow:1,flexBasis:180,borderBottomWidth:1,borderBottomColor:'#ccc'}}>
           <Text>背景 歌词区</Text>
         </View>
-        <View style={{height:60}}>
-           <Text>按钮区：收藏 下载 </Text>
+        <View style={{height:50,flexDirection:'row',alignItems:'center',justifyContent:'space-around',borderBottomWidth:1,borderBottomColor:'#ccc'}}>
+           <FontAwesome name="heart-o" size={30} />
+           <FontAwesome name="arrow-circle-o-down" size={30} />
         </View>
-        <View style={{height:50}}>
-           <Text>时间进度条区: 当前时间- 进度条 - 总时长</Text>
+        <View style={{height:50,flexDirection:'row',alignItems:'center',paddingLeft:5,paddingRight:5,borderBottomWidth:1,borderBottomColor:'#ccc'}}>
+           <Text>00:00</Text>
+           <Slider
+            ref='slider'
+            style={{flex: 1, marginLeft: 4, marginRight: 4}}
+            value={this.state.sliderValue}
+            maximumValue={5}
+            step={1}
+            minimumTrackTintColor='#FFDB42'
+            />
+            <Text>04:30</Text>
         </View>
         <BottomOperateArea />
       </ViewPort>
     )
   }
 }
+// (value) => {
+                // this.refs.video.seek(value / 1000)
+                // // 判断是否处于播放状态			
+                // if (this.state.playButton === 'pause-circle') this.setState({videoPause: false})
+                // }
+            //     onValueChange={}
+            // onSlidingComplete={}
+// this.setState({
+//     videoPause: true,
+//     current: this._formatTime(Math.floor(value / 1000))
+//     })
+// }
+            
 // <Text>歌曲操作区： 随机/单曲、顺序 上一首 暂停/开始 下一首 列表</Text>
 //     <FontAwesome name="heart" size={20} color="red" />
 //     <FontAwesome name="download" size={20} />
@@ -88,11 +114,11 @@ const IconBtn = (props) => (
 const styles = StyleSheet.create({
   bottomContainer: {
     // bottom: 0 ,
-    height: 60,
+    height: 80,
     flexDirection: 'row',
     justifyContent: 'center',
-    paddingBottom: 20,
-    backgroundColor: '#ccc',
+    // paddingBottom: 10,
+    // backgroundColor: '#ccc',
     alignItems: 'center',
   },
 })
